de33bb279d5d832d8c09f8623620f2138336e344
ayod3l3 <ayogundele@gmail.com>
2024-04-10 18:28:54 +0100

    My first commit

--- model/finance_data.py
+++ model/finance_data.py
@@ -1,3 +1,12 @@
-apple
-oranges 
-bannasgit config --global user.name
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import sklearn
+import pandas as pd
+
+
+#Import Training data
+training_data=pd.read_csv('/Users/ayodeleogundele/Desktop/data_science/ANN_pjt/data/as1-bank.csv')
+
+print(training_data.head)
